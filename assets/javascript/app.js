dd492c16a47d5eaab7ac572dcaae18ddb80cbef7
Harrison Palmer <harrisonpalmer@cpe-172-100-3-77.twcny.res.rr.com>
2018-11-05 14:52:35 -0600

    final update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,7 +17,55 @@ var singleTopic = "";
         a.text(topics[i]);
         $("#topics-view").append(a);
     };
-  }
+    $("button").on("click", function() {
+      var animal = $(this).attr("data-name");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=1Ap9PRfNxbH1S8pDXRJkIkh2mwOKmiPR&limit=10";
+        event.preventDefault();
+        
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function(response) {
+          var results = response.data;
+  
+          for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div1>");
+  
+            var rating = results[i].rating;
+            
+            var p = $("<p>").text("Rating: " + rating);
+  
+            var animal = $("<img>");
+            //animal.attr("src", results[i].images.fixed_height.url);
+            animal.attr("src", results[i].images.original_still.url);
+            animal.attr("data-still", results[i].images.original_still.url)
+            animal.attr("data-animate", results[i].images.original.url)
+            animal.attr("data-state", "still");
+            animal.addClass("img-responsive center-block gif")
+  
+            gifDiv.prepend(p);
+            gifDiv.prepend(animal);
+          
+  
+            $("#gif-pic").prepend(gifDiv);
+            $("#rating").html(response);
+          }
+          $("img").on("click", function() {
+            console.log("test"); 
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+              } else if (state === "animate") {
+                $(this).attr("src", $(this).attr("data-still"))
+                $(this).attr("data-state", "still");
+              }});
+              
+      })
+    }) 
+  }  
   
   renderButtons();
   
@@ -97,7 +145,7 @@ $("#add-topic").on("click", function(event){
  // B();
 
 //function B () {
-$("button").on("click", function() {
+/*$("button").on("click", function() {
     var animal = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
       animal + "&api_key=1Ap9PRfNxbH1S8pDXRJkIkh2mwOKmiPR&limit=10";
@@ -146,7 +194,7 @@ $("button").on("click", function() {
     })
   }) 
 
-  
+  */
   
   
 
